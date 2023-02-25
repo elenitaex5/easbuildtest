85ae9aed0afb0276d52a09a405b39350dad973a1
elenitaex5 <elena.martincastillo@abi.ai>
2023-02-25 19:20:17 +0100

    feat(push-notification): finish android build for push notifications. Having error with projectId

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+  android: {
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+  },
+  extra: {
+    eas: {
+      projectId: 'afec2f33-fd70-463b-a0af-1ff17c1a8143'
+    }
+  }
+}
