a75965bdf3ddd4fc779862c4f763c9521cfebf2e
elenitaex5 <elena.martincastillo@abi.ai>
2023-03-01 11:05:44 +0100

    chore(icons): updating app icons and status bar

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ export default function App() {
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
-        <StatusBar backgroundColor={'black'} />
+        <StatusBar />
       </SafeAreaProvider>
     );
   }
