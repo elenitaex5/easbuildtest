8098368e61609cdb6323bae09f97b08223477836
elenitaex5 <elena.martincastillo@abi.ai>
2023-03-02 22:37:02 +0100

    feat(push-notifications): having push notifications as wrapper inside app

---
+++ src/wrappers/PushNotificationWrapper/index.tsx
@@ -0,0 +1,69 @@
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect, useRef } from 'react';
+import { Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    //It must works in prod
+    //token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    /* FIX FOR DEV */
+    const appConfig = require('../../../app.json');
+    const projectId = appConfig?.expo?.extra?.eas?.projectId;
+    const token = (await Notifications.getExpoPushTokenAsync({
+	    projectId
+    })).data;
+
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+  return token;
+}
+
+export const PushNotificationWrapper = () => {
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return <></>
+}
+
+export default PushNotificationWrapper
