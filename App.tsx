cb3a6a20edc5ce0bf52b7ebbabac155c39408cbf
elenitaex5 <elena.martincastillo@abi.ai>
2023-03-03 10:19:56 +0100

    refactor(): formating all project

--- App.tsx
+++ App.tsx
@@ -1,27 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider } from 'native-base'
 
-import useCachedResources from './src/hooks/useCachedResources';
-import useColorScheme from './src/hooks/useColorScheme';
-import Navigation from './src/navigation';
-import PushNotificationWrapper from './src/wrappers/PushNotificationWrapper';
+import useCachedResources from './src/hooks/useCachedResources'
+import useColorScheme from './src/hooks/useColorScheme'
+import Navigation from './src/navigation'
+import PushNotificationWrapper from './src/wrappers/PushNotificationWrapper'
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources()
+  const colorScheme = useColorScheme()
 
   if (!isLoadingComplete) {
-    return null;
+    return null
   } else {
     return (
       <NativeBaseProvider>
         <PushNotificationWrapper />
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-
-     </NativeBaseProvider>
-
-
-    );
+      </NativeBaseProvider>
+    )
   }
 }
--- src/wrappers/PushNotificationWrapper/index.tsx
+++ src/wrappers/PushNotificationWrapper/index.tsx
@@ -1,42 +1,42 @@
-
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import React, { useEffect } from 'react';
-import { Platform } from 'react-native';
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+import React, { useEffect } from 'react'
+import { Platform } from 'react-native'
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: true,
-  }),
-});
+    shouldSetBadge: true
+  })
+})
 
 async function registerForPushNotificationsAsync() {
-  let token;
+  let token
   if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus
     if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
+      const { status } = await Notifications.requestPermissionsAsync()
+      finalStatus = status
     }
     if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
+      alert('Failed to get push token for push notification!')
+      return
     }
     //TODO It must works in prod
     //token = (await Notifications.getExpoPushTokenAsync()).data;
 
     /* FIX FOR DEV */
-    const appConfig = require('../../../app.json');
-    const projectId = appConfig?.expo?.extra?.eas?.projectId;
-    token = (await Notifications.getExpoPushTokenAsync({
-	    projectId
-    })).data;
-
+    const appConfig = require('../../../app.json')
+    const projectId = appConfig?.expo?.extra?.eas?.projectId
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId
+      })
+    ).data
   } else {
-    alert('Must use physical device for Push Notifications');
+    alert('Must use physical device for Push Notifications')
   }
 
   if (Platform.OS === 'android') {
@@ -44,16 +44,16 @@ async function registerForPushNotificationsAsync() {
       name: 'default',
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
+      lightColor: '#FF231F7C'
+    })
   }
-  return token;
+  return token
 }
 
 export const PushNotificationWrapper = () => {
   useEffect(() => {
     registerForPushNotificationsAsync()
-  }, []);
+  }, [])
 
   return <></>
 }
