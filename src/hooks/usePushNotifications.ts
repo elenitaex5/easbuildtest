59e4af2eacc24f6a81a026545337542f6127d857
elenitaex5 <elena.martincastillo@abi.ai>
2023-03-04 11:28:19 +0100

    refactor(push-notification): refactroing push token function

--- src/hooks/usePushNotifications.ts
+++ src/hooks/usePushNotifications.ts
@@ -19,14 +19,13 @@ async function registerForPushNotificationsAsync() {
   }
 
   const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
   if (existingStatus !== 'granted') {
     const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
-  }
-  if (finalStatus !== 'granted') {
-    alert('Failed to get push token for push notification!');
-    return;
+
+    if (status !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
   }
   //TODO It must works in prod
   //token = (await Notifications.getExpoPushTokenAsync()).data;
